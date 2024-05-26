1cbc3cba14b55d7fdaa43ea0cdacee9ecba9be99
Ajith Sritharan <sritharanajith04@gmail.com>
2024-05-26 20:32:30 +0530

    final

---
+++ script.js
@@ -0,0 +1,324 @@
+// // print stars in horizontal
+// for (i=1; i<=5; i++){
+//     document.write("*" + " ");
+// }
+
+// // print stars in vertical
+// for (i=1; i<=5; i++){
+//     document.write("*" + "<br>");
+// }
+
+// // print stars in square
+// for (i=1; i<=5; i++){
+// for (j=1; j<=5; j++){
+//     document.write("*" + " ");
+// }
+// document.write("<br>");
+// }
+
+// // Triangle using single loop
+// var print = "";
+// for (i=1; i<=5; i++){
+//     print = print + "*" + " ";
+//     document.write(print + "<br>")
+// }
+
+// // Triangle-1
+// for (i=1; i<=5; i++){
+//     for (j=1; j<=i; j++){
+//         document.write("*" + " ");
+//     }
+//     document.write("<br>");
+// }
+
+// // Triangle-1 star increases +2
+// for (i=1; i<=5; i+=2){
+//     for (j=1; j<=i; j++){
+//         document.write("*" + " ");
+//     }
+//     document.write("<br>");
+// }
+
+// // triangle-1 upside down
+// for (i=5; i>=1; i--){
+//     for (j=1; j<=i; j++){
+//         document.write("*" + " ");
+//     }
+//     document.write("<br>");
+// }
+
+
+let n = 5; // row or column count
+// defining an empty string
+let string = "";
+
+for(let i = 0; i < n; i++) { // external loop
+  for(let j = 0; j < n; j++) { // internal loop
+    if(i === 0 || i === n - 1) {
+      string += "*";
+    }
+    else {
+      if(j === 0 || j === n - 1) {
+        string += "*";
+      }
+      else {
+        string += " ";
+      }
+    }
+  }
+  // newline after each row
+  string += "<br>";
+}
+// printing the string
+document.write(string);
+
+
+
+
+
+// let n = 5;
+// let string = "";
+// for (let i = 1; i <= n; i++) {
+//   // printing spaces
+//   for (let j = 0; j < n - i; j++) {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = 0; k < i; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+
+
+// let n = 6;
+// let string = "";
+
+// for (let i = 1; i <= n; i++) {
+//   // printing star
+//   for (let j = 0; j < i; j++) {
+//     if(i === n) {
+//       string += "*";
+//     }
+//     else {
+//       if (j == 0 || j == i - 1) {
+//         string += "*";
+//       }
+//       else {
+//         string += " ";
+//       }
+//     }
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+
+
+// let n = 5;
+// let string = "";
+// // External loop
+// for (let i = 1; i <= n; i++) {
+//   // printing spaces
+//   for (let j = 1; j <= n - i; j++) {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = 0; k < 2 * i - 1; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+
+
+// let n = 5;
+// let string = "";
+
+// // External loop
+// for (let i = 1; i <= n; i++) {
+//   // printing spaces
+//   for (let j = 1; j <= n - i; j++) {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = 0; k < 2 * i - 1; k++) {
+//     if(i === 1 || i === n) {
+//       string += "*";
+//     }
+//     else {
+//       if(k === 0 || k === 2 * i - 2) {
+//         string += "*";
+//       }
+//       else {
+//         string += " ";
+//       }
+//     }
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+
+
+// let n = 5;
+// let string = "";
+// // Upside pyramid
+// for (let i = 1; i <= n; i++) {
+//   // printing spaces
+//   for (let j = n; j > i; j--) {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = 0; k < i * 2 - 1; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// // downside pyramid
+// for (let i = 1; i <= n - 1; i++) {
+//   // printing spaces
+//   for (let j = 0; j < i; j++) {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = (n - i) * 2 - 1; k > 0; k--) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+
+
+// let n = 5;
+// let string = "";
+// // Upside pyramid
+// // upside diamond
+// for (let i = 1; i <= n; i++) {
+//   // printing spaces
+//   for (let j = n; j > i; j--) {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = 0; k < i * 2 - 1; k++) {
+//     if (k === 0 || k === 2 * i - 2) {
+//       string += "*";
+//     }
+//     else {
+//       string += " ";
+//     }
+//   }
+//   string += "\n";
+// }
+// // downside diamond
+// for (let i = 1; i <= n - 1; i++) {
+//     // printing spaces
+//     for (let j = 0; j < i; j++) {
+//       string += " ";
+//     }
+//     // printing star
+//     for (let k = (n - i) * 2 - 1; k >= 1; k--) {
+//       if (k === 1 || k === (n - i) * 2 - 1) {
+//         string += "*";
+//       }
+//       else {
+//         string += " ";
+//       }
+//     }
+//     string += "\n";
+//   }
+// console.log(string);
+
+
+
+
+
+// let n = 5;
+// let string = "";
+// // Reversed pyramid pattern
+// for (let i = 0; i < n; i++) {
+//   // printing spaces
+//   for (let j = 0; j < i; j++) {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = 0; k < (n - i) * 2 - 1; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// // pyramid pattern
+// for (let i = 2; i <= n; i++) {
+//   // printing spaces
+//   for (let j = n; j > i; j--) {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = 0; k < i * 2 - 1; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);let n = 5;
+// let string = "";
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// for (let i = 1; i <= n - 1; i++) {
+//   for (let j = 0; j < n - i; j++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+
+
+
+// var n = 6;
+// var str = "";
+// for (let i = n / 2; i < n; i += 2) {
+//   // print first spaces
+//   for (let j = 1; j < n - i; j += 2) {
+//     str += " ";
+//   }
+//   // print first stars
+//   for (let j = 1; j < i + 1; j++) {
+//     str += "*";
+//   }
+//   // print second spaces
+//   for (let j = 1; j < n - i + 1; j++) {
+//     str += " ";
+//   }
+//   // print second stars
+//   for (let j = 1; j < i + 1; j++) {
+//     str += "*";
+//   }
+//   str += "\n";
+// }
+// // lower part
+// // inverted pyramid
+// for (let i = n; i > 0; i--) {
+//   for (let j = 0; j < n - i; j++) {
+//     str += " ";
+//   }
+//   for (let j = 1; j < i * 2; j++) {
+//     str += "*";
+//   }
+//   str += "\n";
+// }
+// console.log(str);
+
